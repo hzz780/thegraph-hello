9025c67f435fbd15e6f3b63eae53c320ac8b0692
hzz780 <hzz780@qq.com>
2021-03-03 18:49:03 +0800

    ci: 🎡 基本操作&反向查询&多网络部署 示例

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # Example Subgraph
 
 An example to help you get started with The Graph. For more information see the docs on https://thegraph.com/docs/.
+
+## 1
+
+```javascript
+
+  1. Run `graph auth https://api.thegraph.com/deploy/ <access-token>`
+     to authenticate with the hosted service. You can get the access token from
+     https://thegraph.com/explorer/dashboard/.
+
+  2. Type `cd hzzhelloworld` to enter the subgraph.
+
+  3. Run `yarn deploy` to deploy the subgraph to
+     https://thegraph.com/explorer/subgraph/hzz780/hzzhelloworld.
+```
+
+## 2
+
+graph init --from-example hzz780/hzzhelloworld hzzhelloworld
+
+```javascript
+graph deploy \      
+    --debug \
+    --node https://api.thegraph.com/deploy/ \
+    --ipfs https://api.thegraph.com/ipfs/ \
+    hzz780/hzzhelloworld
+```
+
+## 3. 日常维护
+
+```text
+看package.json的script
+yarn codegen
+yarn build
+yarn deploy
+```
+
+## 4. 区分网络部署
+
+```text
+https://thegraph.com/docs/deploy-a-subgraph#deploying-the-subgraph-to-multiple-ethereum-networks
+
+见 config文件内容和 subgraph.template.yaml
+以及 yarn deploy:mainnet 示例
+```
+
+
+
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,15 +1,29 @@
-import { NewGravatar, UpdatedGravatar } from '../generated/Gravity/Gravity'
-import { Gravatar } from '../generated/schema'
+import {NewGravatar, UpdatedGravatar} from '../generated/Gravity/Gravity'
+import {Gravatar, GravatarImage} from '../generated/schema'
+import { log, BigInt } from '@graphprotocol/graph-ts'
 
 export function handleNewGravatar(event: NewGravatar): void {
   let gravatar = new Gravatar(event.params.id.toHex())
   gravatar.owner = event.params.owner
   gravatar.displayName = event.params.displayName
   gravatar.imageUrl = event.params.imageUrl
+  gravatar.zzTest = 'hzzTest';
+  gravatar.updateAt = event.block.timestamp;
+  gravatar.createdAt = event.block.timestamp;
+  gravatar.imageId = updateGravatarImage(true);
+
   gravatar.save()
 }
 
 export function handleUpdatedGravatar(event: UpdatedGravatar): void {
+
+  // https://thegraph.com/docs/assemblyscript-api#logging-and-debugging
+  // log.info('Message to be displayed: {}, {}, {}', [
+  //   event.block.timestamp.toString(),
+  //   event.block.number.toString(),
+  //   'already a string',
+  // ]);
+
   let id = event.params.id.toHex()
   let gravatar = Gravatar.load(id)
   if (gravatar == null) {
@@ -18,5 +32,30 @@ export function handleUpdatedGravatar(event: UpdatedGravatar): void {
   gravatar.owner = event.params.owner
   gravatar.displayName = event.params.displayName
   gravatar.imageUrl = event.params.imageUrl
+  gravatar.zzTest = 'hzzTest';
+  gravatar.updateAt = event.block.timestamp;
+  gravatar.createdAt = event.block.timestamp;
+  gravatar.imageId = updateGravatarImage(false);
+
   gravatar.save()
+}
+
+// 不可以使用 isNew?: boolean, 必须是明确的值
+function updateGravatarImage(isNew: boolean): string {
+  let id = '100';
+
+  let gravatarImage = GravatarImage.load(id);
+  if (gravatarImage === null) {
+    gravatarImage = new GravatarImage(id);
+    gravatarImage.count = BigInt.fromI32(1);
+    gravatarImage.type = 'blue';
+    gravatarImage.save();
+  }
+  // graphql 没有count的概念，如果需要处理分页，这样做会比较合适。
+  if (isNew) {
+    gravatarImage.count = gravatarImage.count.plus(BigInt.fromI32(1));
+    gravatarImage.type = 'others';
+    gravatarImage.save();
+  }
+  return gravatarImage.id;
 }
